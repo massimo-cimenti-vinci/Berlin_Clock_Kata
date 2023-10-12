0daef588c1b20ac82fdd8e134ef2335030cc4960
massimo-cimenti-vinci <massimo.cimenti@student.vinci.be>
2023-10-12 17:42:09 +0200

    add code base

---
+++ spec/appSpec.js
@@ -0,0 +1,13 @@
+import { Main } from '../src/app.js';
+
+describe("A suite of test for the demo function of the main class", function() {
+    let main = new Main();
+    
+    it("", function() {
+        //let result = main.berlinClock(1);
+
+        //expect(result).toBe(1);
+    });
+    
+  
+});
